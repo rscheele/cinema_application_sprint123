f0d88119f4b97199531e1778e52c570da1b1b1f1
Rodi <rscheele@clubpellikaan.nl>
2018-03-09 14:38:18 +0100

    added reservation id and generation based on time

---
+++ Sprint123/UnitTests/ReservationID.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ReservationID
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DateTime dateTime = new DateTime();
+            int year = dateTime.Year;
+            int doy = dateTime.DayOfYear;
+            int ms = dateTime.Millisecond;
+            int reservationID = int.Parse(year.ToString() + doy.ToString() + ms.ToString());
+            Debug.WriteLine(reservationID);
+        }
+    }
+}
